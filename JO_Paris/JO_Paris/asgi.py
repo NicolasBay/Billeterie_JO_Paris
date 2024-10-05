32c2ba73a7f37e5080a231aeff554c76784b962d
NicolasBay <nicolasbaylou@gmail.com>
2024-10-05 19:02:43 +0400

    Initial commit

---
+++ JO_Paris/JO_Paris/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for JO_Paris project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JO_Paris.settings')
+
+application = get_asgi_application()

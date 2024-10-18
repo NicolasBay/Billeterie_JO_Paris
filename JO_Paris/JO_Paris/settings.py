97b06ec9dbf7d067d468973bbc49f6a55ab35468
NicolasBay <nicolasbaylou@gmail.com>
2024-10-19 00:24:50 +0400

    setting.py  pour déploiement Heroku

--- JO_Paris/JO_Paris/settings.py
+++ JO_Paris/JO_Paris/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -56,6 +57,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MESSAGE_LEVEL = messages.INFO
 
 ROOT_URLCONF = 'JO_Paris.urls'
@@ -91,6 +95,11 @@ DATABASES = {
         'DATABASE_URL',
         cast=db_url
     )
+}
+
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # Ajouter les options spécifiques pour MySQL
@@ -145,6 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/billetterie/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / "billetterie/static"),
 ]
